c69f8bcc3db87408cec6c289ed5275a30ea60c39
Pritam <spritam675@gmail.com>
2022-12-12 20:21:12 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static void main(String[] args) {
+        // B b=new A() --> child cannot hold parent object
+        //here we have to call meth of A
+        B b=new B();
+        b.meth();
+    }
   
 }
